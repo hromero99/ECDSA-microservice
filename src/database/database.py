ccf493392d24b3325c8b4645af0956390ee1e7c1
Héctor Romero López <hector@hromerol.es>
2023-07-13 12:09:10 +0200

    ADD: Connection for postgres database

--- src/database/database.py
+++ src/database/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os 
+from pydantic import PostgresDsn
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URI = PostgresDsn.build(
+    scheme="postgresql",
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    host=os.getenv("DATABASE_SERVER"),
+    path=f"/{os.getenv('DATABASE_DB')}",
+)
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URI
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
